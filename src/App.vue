a2abe4003dfdc7d9c30d89c99ef196c9719725d0
myhub-ljc <2321170474@qq.com>
2020-03-05 18:03:04 +0800

    tabbar基本完成

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    
+    <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
+import MainTabBar from 'components/content/maintabbar/MainTabBar.vue'
+
 export default {
   name: 'App',
-  components: {
-    
+  components: {  
+    MainTabBar
   }
 }
 </script>
 
 <style>
-
+@import 'assets/css/base.css';
 </style>
